0afffcc62574aa75ac55b62deb90d5b7c5c91775
Praveen9329 <sindegeripraveenkumar336@gmail.com>
2023-11-03 12:25:35 +0530

    Day 03

--- Adeeba1.sh
+++
@@ -1,6 +0,0 @@
-xread -p "Enter the Number: " u
-read -p "Enter the Number: " v
-if [ $u -gt $v ]
-then
-echo $u is greater than $v
-fi
--- Adeeba2.sh
+++
@@ -1,6 +0,0 @@
-read -p "Enter the 1st Number: " m
-read -p "Enter the 2nd Number: " n
-echo $m
-echo $n
-j=$(( $m - $n ))
-echo $j
